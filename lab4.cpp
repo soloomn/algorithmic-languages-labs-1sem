22be0f2101bd33c37f6a8fb052afe72d9c1921e3
Timofey Smetanin <resn2p@gmail.com>
2024-01-21 01:42:53 +0300

    laboratory work No. 4

--- lab4.cpp
+++ lab4.cpp
@@ -22,7 +22,7 @@ struct TableString
 	//a string containing two ternary digits (the string ends with '\0')
 };
 
-int mainlab4()
+int main()
 {
 	char ch;   //is used for character-by-character input
 	int  n = 0, k = 0;  // character counters
@@ -30,27 +30,27 @@ int mainlab4()
 	char triple[81]; // ternary number
 	char zero[81];  //a line at the beginning of which '0' will be written
 	char tmp[5];
-	// ���� ����� ������������ �������� �������� ���� �� ������ bin
-	strcpy_s(zero, "0"); // ���������� ���� � ������ ������
-	cout << "������� 2-�� �����: \n";
-	while ((ch = _getch()) != '\r') // ���� �� ������ ������� <Enter>
+	// the two ternary digits from the "triple" string will be copied here
+	strcpy_s(zero, "0"); // write zero at the beginning of the line
+	cout << "Enter a ternary number: \n";
+	while ((ch = _getch()) != '\r') // when the <Enter> key is not pressed
 	{
-		if ((ch == '0' || ch == '1' || ch == '2') && k < 79) // ���� ������� 2-� �����
+		if ((ch == '0' || ch == '1' || ch == '2') && k < 79) // if a ternary digit is entered
 		{
 			cout << ch; triple[k++] = ch;
-			// ������� �� �� ����� � ���������� � ������
+			// display it on the screen and write it to a string
 		}
-	}  //end while
+	}
 	triple[k] = 0; enea[n] = 0; cout << endl;
-	//������ ����� ������ ������� 4:
+	//make the line length divisible by 2:
 	while (strlen(triple) % 2)
 	{
-		strcpy(triple, strcat(zero, triple)); /* ���������� ������, ���������� �0�, � bin, ����� ���������� ����� �������� � bin, � ���������� � ������ bin ����������� �0� */
-		strcpy(zero, "0");  /* ��������������� zero, ��� ������ ��������� ������ 1 ����*/
+		strcpy(triple, strcat(zero, triple)); /* add the string containing '0' to "triple", then copy the sum to "triple", as a result '0' is added to the beginning of "triple".*/
+		strcpy(zero, "0");  /* restore "zero", it must contain only 1 zero */
 	}
-	cout << triple << endl; // ��������, ��� ����������
-	// �������:
-	TableString table[16] = { { '0', "0" },  /* �������� �������� �� ��, ��� ������// ���������� ������ �������� */
+	cout << triple << endl; // let's see what we got
+	// Translation:
+	TableString table[16] = { { '0', "00" },  /* note how the array of structures is initialized */
 	{ '1', "01" },
 	{ '2', "02" },
 	{ '3', "10" },
@@ -59,23 +59,23 @@ int mainlab4()
 	{ '6', "20" },
 	{ '7', "21" },
 	{ '8', "22" } };
-	// �������� �� 2-�� �����, ����������� �� 4 �����:	
+	// pass through the ternary number, processing two digits at a time:	
 	for (int i = 0; i<strlen(triple); i += 2)
 	{
-		strncpy(tmp, triple + i, 2); // �������� ��������� 4 ����� � tmp
-		tmp[2] = '\0';   // strncpy() ������� ����� ������ �� ����������
-		for (int j = 0; j<9; j++) //  ��������� ����� � �������
-			if (!strcmp(table[j].triple, tmp)) // ���� strcmp() ������� 0, ������ tmp ������� � i-�� ������� �������
+		strncpy(tmp, triple + i, 2); // copy the next two digits to "tmp"
+		tmp[2] = '\0';   // strncpy() end of string sign - no writing will be done
+		for (int j = 0; j<9; j++) //  search the table
+			if (!strcmp(table[j].triple, tmp)) // if strcmp() returns 0, then "tmp" matches the i-th row of the table
 			{
-				enea[n++] = table[j].enea;/* �������� 16-� ����� �� ������� � ������ hex */
+				enea[n++] = table[j].enea;/* copy the nonary digit from the table to the "hex" string */
 				break;
 			}
 	}
 	
 	enea[n] = '\0';
-	cout << "��������������� 16-�� ����� = \n";
+	cout << "The corresponding nonary number =\n";
 	cout << enea << endl;
 	_getch(); //the standard windows terminal sometimes closes automatically after code execution, with the empty command "_getch()"
-	//let's put it in "standby mode" before entering any character via "enter"
+	//let's put it in "standby mode" before entering any character via <Enter> (if you use the standard MS VS Code terminal, this command can be removed)
 	return 0;
 }
